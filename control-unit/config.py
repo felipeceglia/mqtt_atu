9ba275c0ee5c7c1a069cca1dc59c98e868a10541
Felipe <github@dxwatch.com>
2022-11-16 18:10:16 +0000

    added speed control

--- control-unit/config.py
+++ control-unit/config.py
@@ -16,19 +16,26 @@ MQTT_PING_PERIOD = 5000 #ms
 MSG_LEVEL = 'debug'
 
 BUTTONS = [
-    {'name':'L_up1', 'dst':SLAVE_ID, 'action':'L', 'pin': 10, 'val': 1},
-    {'name':'L_up2', 'dst':SLAVE_ID, 'action':'L', 'pin': 11, 'val': 2},
-    {'name':'L_dwn1','dst':SLAVE_ID, 'action':'L', 'pin': 12, 'val':-1},
-    {'name':'L_dwn2','dst':SLAVE_ID, 'action':'L', 'pin': 13, 'val':-2},
+    {'name':'L_up_slow',  'dst':SLAVE_ID, 'action':'L', 'pin': 10, 'msg': 'up|', 'speed':50, 'adc_pin': 28},
+    {'name':'L_up2_fast', 'dst':SLAVE_ID, 'action':'L', 'pin': 11, 'msg': 'up|', 'speed':100},
+    {'name':'L_dwn_slow', 'dst':SLAVE_ID, 'action':'L', 'pin': 12, 'msg': 'dwn|','speed':50, 'adc_pin': 28},
+    {'name':'L_dwn_fast', 'dst':SLAVE_ID, 'action':'L', 'pin': 13, 'msg': 'dwn|','speed':100},
 
-    {'name':'L_xtr1', 'dst':SLAVE_ID, 'action':'L_xtr1', 'pin': 9, 'val':2},
+    {'name':'L_xtr1', 'dst':SLAVE_ID, 'action':'L_xtr1', 'pin': 9, 'msg':2},
 
-    {'name':'C_up1', 'dst':SLAVE_ID, 'action':'C',  'pin': 2, 'val': 1},
-    {'name':'C_up2', 'dst':SLAVE_ID, 'action':'C',  'pin': 3, 'val': 2},
-    {'name':'C_dwn1','dst':SLAVE_ID, 'action':'C', 'pin': 4, 'val':-1},
-    {'name':'C_dwn2','dst':SLAVE_ID, 'action':'C', 'pin': 5, 'val':-2},
+    {'name':'C_up1', 'dst':SLAVE_ID, 'action':'C',  'pin': 2, 'msg': 1},
+    {'name':'C_up2', 'dst':SLAVE_ID, 'action':'C',  'pin': 3, 'msg': 2},
+    {'name':'C_dwn1','dst':SLAVE_ID, 'action':'C', 'pin': 4, 'msg':-1},
+    {'name':'C_dwn2','dst':SLAVE_ID, 'action':'C', 'pin': 5, 'msg':-2},
     
-    {'name':'C_xtr1', 'action':'C_xtr1', 'pin': 8, 'val':2},
+    {'name':'C_xtr1', 'action':'C_xtr1', 'pin': 8, 'msg':2},
 
-#    {'name':'L_up',   'pin': 13, 'val':1},
+#    {'name':'L_up',   'pin': 13, 'msg':1},
 ]
+
+ADC_SPEED = {
+    'L': { 'pin': 28, 'pct_step':2, 'pct_min':10, 'adc_max':65100, 'dst':SLAVE_ID },
+    }
+
+
+    
--- control-unit/main.py
+++ control-unit/main.py
@@ -6,7 +6,7 @@ import time
 import urequests
 import utime
 
-
+from math import ceil
 from machine import Pin
 from umqtt.robust2 import MQTTClient
 
@@ -22,6 +22,7 @@ led_board.value(1)
 
 echo('starting... CONTROL UNIT')
 
+##############################################################
 
 def wifi_connect(ssid, passwd):
     #global config
@@ -43,27 +44,21 @@ def wifi_connect(ssid, passwd):
 
             status = sta_if.status()
 
-            try:
-                with open('errors.txt', 'a') as outfile:
-                    outfile.write('connect status = ' + str(status) + '\n')
-            except OSError:
-                print('oops')
+            #try:
+            #    with open('errors.txt', 'a') as outfile:
+            #        outfile.write('connect status = ' + str(status) + '\n')
+            #except OSError:
+            #    print('oops')
 
             if (sta_if.isconnected()):
                 count = 0
                 break
 
-            print('.', end = '')
+            #print('.', end = '')
             utime.sleep(1)
 
     if (count == 5):
         count = 0
-
-#         try:
-#             with open('errors.txt', 'a') as outfile:
-#                 outfile.write('did NOT connect to internet' + '\n')
-#         except OSError:
-#             print('oops')
 
         echo(f'cannot connect to wifi {ssid}, resetting')
         utime.sleep(2)
@@ -76,22 +71,22 @@ def wifi_connect(ssid, passwd):
 wifi_connect(config_wifi.SSID, config_wifi.PASSWORD)
 
 ##############################################################
-# echo (f'connecting to wifi: {config_wifi.SSID}...')
-# wlan = network.WLAN(network.STA_IF)
-# wlan.active(True)
-# wlan.connect(config_wifi.SSID, config_wifi.PASSWORD)
-# time.sleep(5)
-# if wlan.isconnected():
-#     echo (f'connected to wifi: {config_wifi.SSID}')
-##############################################################    
+
+def get_pin_adc(pin):
+    global adcs_map 
+
+    adc = adcs_map[pin]
     
-sensor = Pin(16, Pin.IN)
-
-#TODO
-
-state = 0
+    return( adc_to_pct(adc['m'].read_u16(), adc['pct_step'], adc['adc_max']))
 
 ##############################################################
+#converts adc reading from int to percent
+
+def adc_to_pct(adc_in, round_to=5, adc_max=65100):
+    return ( ceil( (int(100 * adc_in / adc_max) / round_to) ) * round_to) 
+
+##############################################################
+
 def mqtt_sub_cb(topic, msg, retained, duplicate):
     #echo((topic, msg, retained, duplicate))
     topic = topic.decode("utf-8").split('/')
@@ -101,10 +96,9 @@ def mqtt_sub_cb(topic, msg, retained, duplicate):
        RTL = utime.ticks_ms() - int(msg)
        echo(('ping RTL is:', str(RTL)))
        mqtt_client.publish('/CT7ANO/atu/info/control_unit/remote_unit/ping_RTL', str(RTL))
-    
-    #print(utime.ticks_ms())
 
 ##############################################################
+
 def mqtt_connect():
     global mqtt_client
     global config
@@ -115,7 +109,6 @@ def mqtt_connect():
             #keepalive=3600,
     )
     
-    
     mqtt_client.DEBUG = True
     mqtt_client.KEEP_QOS0 = False
     # Option, limits the possibility of only one unique message being queued.
@@ -124,12 +117,7 @@ def mqtt_connect():
     mqtt_client.MSG_QUEUE_MAX = 2
 
     mqtt_client.set_callback(mqtt_sub_cb)
-    
-    #mqtt_client.connect()
-    
-    #if not mqtt_client.connect(clean_session=False):
-    #    print("New session being set up")
-    
+        
     sub = config.MQTT_TOPIC_SUB + b'/#'
     mqtt_client.subscribe(sub)
     
@@ -143,81 +131,109 @@ def mqtt_connect():
     
     return mqtt_client
 
-##############################################################  
+##############################################################
+
 def mqtt_reconnect():
     echo('Failed to connect to the MQTT Broker. Reconnecting...')
     led_mqtt.value(0)
     time.sleep(5)
     machine.reset()
 
-
 ##############################################################  
 # This function gets called every time the button is pressed.  The parameter "pin" is not used.
+
 def btn_handler(p):
 
     #ugly hack, but kind of works
     pin = int(str(p)[4:6].rstrip(','))
     
-    global btn_last_time
+
     #global btn_presses
     global btn_map
 
     new_time = utime.ticks_ms()
-    elapsed_time = new_time - btn_last_time[pin]
+    elapsed_time = new_time - btn_map[pin]['last_time']
     
     # if it has been more that 1/5 of a second since the last event, we have a new event
-    if elapsed_time > 75:
+    if elapsed_time > 80:
         b = btn_map[pin]
         
         if elapsed_time < 500:
-            print('meio')
+            #print('meio')
             if b['val'] != b['last_val']:
                 echo('let it coast', 'debug')
         
-        echo(('pressed pin', pin, btn_map[pin]['name'], 'state', state, 'elapsed', elapsed_time, 'value', p.value()), 'debug')
-        #btn_presses[pin] +=1
-        btn_last_time[pin] = new_time
+        echo(('pressed pin', pin, btn_map[pin]['name'], 'elapsed', elapsed_time, 'value', p.value()), 'debug')
+        
+        btn_map[pin]['last_time'] = new_time
 
-        mqtt_topic = config.MQTT_TOPIC_PUB + '/' + b['action']
+        #TODO FIXME
+        #error raising here if buton is pressed upon startup
+        mqtt_pub_pin(pin, p.value())
     
-        mqtt_topic = config.MQTT_TOPIC_PUB + '/' + b['dst'] + '/' + config.MASTER_ID + '/' + b['action']
-        if p.value() == 0:
-             mqtt_msg = 0
-        else:
-            mqtt_msg = b['val']
-
-        echo((mqtt_topic, mqtt_msg, b['last_val']))
-        
-        mqtt_client.publish(mqtt_topic, str(mqtt_msg))
-        
-        b['last_val'] = mqtt_msg
-        
         return
 
+##############################################################
+    
+def mqtt_pub_pin(pin, value):
+    global pins_active
+    #print(pin,value)
+    
+    b = btn_map[pin]
+    
+    mqtt_topic = config.MQTT_TOPIC_PUB + '/' + b['dst'] + '/' + config.MASTER_ID + '/' + b['action']
+    
+    if value == 0:
+        mqtt_msg = 'stop|0'
+        pins_active.remove(pin)
+        
+    else:
+        if pin not in pins_active:
+            pins_active.append(pin)
+        
+        #lets get speed value from adc
+        if 'adc_pin' in b:
+            btn_map[pin]['speed'] = get_pin_adc(b['adc_pin'])
+                        
+        mqtt_msg = b['msg'] + str(btn_map[pin]['speed'])
+
+    if mqtt_msg == b['msg_last']:
+        return False
+      
+    mqtt_client.publish(mqtt_topic, str(mqtt_msg))
+    b['msg_last'] = mqtt_msg
+    
 ##############################################################  
-
-
-buttons = config.BUTTONS
 
 btn_map 		= {}
-btn_last_time 	= {}
-btn_last_state 	= {}
-#btn_presses 	= {}
 pins_in 		= {}
-
+pins_active = []
 
 ##############################################################  
-for d in buttons:
+for d in config.BUTTONS:
+    pin = d['pin']
     echo(('setting pin', d['pin'], d['name']), 'debug')
-    btn_map[d['pin']] 		= d
-    btn_map[d['pin']]['last_val'] = 0
-    btn_last_time[d['pin']] = 0
-    btn_last_state[d['pin']]= False
-    #btn_presses[d['pin']] 	= 0
+    btn_map[pin] 		= d
+    btn_map[pin]['val'] = 0
+    btn_map[pin]['last_val'] = 0
+    btn_map[pin]['last_time'] = 0
+    btn_map[pin]['last_speed'] = 0
+    btn_map[pin]['msg_last'] = ''
         
-    pins_in[d['pin']] = machine.Pin(d['pin'], machine.Pin.IN, machine.Pin.PULL_DOWN)
-    pins_in[d['pin']].irq(trigger=machine.Pin.IRQ_FALLING, handler=btn_handler)
+    btn_map[pin]['m'] = machine.Pin(d['pin'], machine.Pin.IN, machine.Pin.PULL_DOWN)
+    btn_map[pin]['m'].irq(trigger=machine.Pin.IRQ_FALLING, handler=btn_handler)
 
+##############################################################  
+adcs		= config.ADC_SPEED
+adcs_map	= {}
+
+
+for name, a in config.ADC_SPEED.items():
+    echo (f'setting ADC {name}: {a}', 'debug')
+    adcs[name]['m'] = machine.ADC(a['pin'])
+    adcs[name]['last_val'] = 0
+    adcs[name]['val'] = 50
+    adcs_map[a['pin']] = a
 ##############################################################  
 
 try:
@@ -238,12 +254,30 @@ i_ping = 0
 
 while True:
     utime.sleep_ms(sleep_ms)
-    
+
     ##############################################################
+   
     i_ping += 1
     if i_ping == ping_interval:
         i_ping = 0
         mqtt_client.publish(config.MQTT_TOPIC_PUB + '/' + config.SLAVE_ID + '/' + config.MASTER_ID + '/ping', str(utime.ticks_ms()))
+
+    ##############################################################
+
+    for pin in pins_active:
+        if pin in btn_map and 'adc_pin' in btn_map[pin]:
+            #print('.',end='')
+            speed = get_pin_adc(btn_map[pin]['adc_pin'])
+            #adc_to_pct(adcs[btn_map[pin]['action']]['m'].read_u16())
+
+            if speed != btn_map[pin]['last_speed']:
+                btn_map[pin]['speed'] = speed
+                
+                mqtt_pub_pin(pin, str(speed) + str(utime.ticks_ms()))
+                
+                btn_map[pin]['last_speed'] = speed
+
+        
     ##############################################################
 
     if mqtt_client.is_conn_issue():
@@ -254,8 +288,11 @@ while True:
         else:
             mqtt_client.resubscribe()
                
+    ##############################################################
+            
     mqtt_client.check_msg() # needed when publish(qos=1), ping(), subscribe()
     mqtt_client.send_queue()  # needed when using the caching capabilities for unsent messages
+
 
 mqtt_client.disconnect()
 led_mqtt.value(0)
